00559a36644697477fcd718dd288da03166b86f6
Brian Feldman <brian.feldman42@gmail.com>
2021-01-04 16:57:08 +0000

    basic landing page added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,16 +1,41 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Header, Divider, Image, Segment } from 'semantic-ui-react'
 
+import bpgrid from '../img/home/bp_grid.png'
+import bpcam from '../img/home/bp_cam.png'
+import bpgif from '../img/home/bp_gif.gif'
 
 function Home() {
   return(
-    <Header
-      style={{
-        fontSize:"70px"
-      }}
-    >
-      BOTO PHOOTH
-    </Header>
+    <div style={{margin:"0 auto", width:"700px"}}>
+      <Header
+        style={{
+          fontSize:"70px"
+        }}
+      >
+        BOTO PHOOTH
+      </Header>
+          
+          <Segment size="huge" color="teal" inverted style={{width:"500px", margin:"0 auto"}}>
+            <Image src={bpgif} size="huge" style={{margin:"0 auto"}}/>
+            <p>Boto Phooth is a web-based, kinda-fully-featured tool for take, editing, and sharing webcam pictures.</p>
+          </Segment>
+          
+          <Divider />
+          
+          <Segment size="huge" color="red" inverted style={{width:"500px", margin:"0 auto"}}>
+            <Image src={bpcam} size="medium" style={{margin:"0 auto"}}/>
+            <p>Head over to the studio to take a picture.</p>
+          </Segment>
+          
+          <Divider />
+          
+          <Segment size="huge" color="blue" inverted style={{width:"500px", margin:"0 auto"}}>
+            <Image src={bpgrid} size="medium" style={{margin:"0 auto"}}/>
+            <p>Use the image editor to make your pic look really cool, then share it with your friends.</p>
+          </Segment>
+            
+    </div>
   )
 }
 
